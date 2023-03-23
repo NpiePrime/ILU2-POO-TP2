419f3ba1996a9727a2ec14319ef8836463471fcc
Yanis Zerkani <zrn3963a@ens.fsi>
2023-03-23 17:34:51 +0100

    AcheterP/afficherM

--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -24,26 +24,30 @@ public class BoundaryAcheterProduit {
 		String nomVendeur;
 		Etal etalVendeur;
 		Gaulois[] vendeurs;
+		int choixUtilisateur = 0;
+
 		System.out.println("Quel produit voulez-vous acheter?");
 		produit = scan.next();
 		vendeurs = controlAcheterProduit.trouverVendeurProduit(produit);
-		int choixUtilisateur = 0;
+		
+		
 		question.append("Chez quel commerçant voulez-vous acheter vos " + produit + " ?\n");
 		for (int i = 0; i < vendeurs.length; i++) {
-			question.append((i+1) + " - " + vendeurs[i].getNom()+"\n");
+			question.append((i + 1) + " - " + vendeurs[i].getNom() + "\n");
 		}
+		
 		do {
 			choixUtilisateur = Clavier.entrerEntier(question.toString());
-		} while (choixUtilisateur == 0 && choixUtilisateur>vendeurs.length);
-		nomVendeur = vendeurs[choixUtilisateur-1].getNom();
+		} while (choixUtilisateur == 0 && choixUtilisateur > vendeurs.length);
+		
+		nomVendeur = vendeurs[choixUtilisateur - 1].getNom();
 		etalVendeur = controlAcheterProduit.trouverEtalVendeur(nomVendeur);
 		System.out.println(nomAcheteur + " se déplace jusqu'à l'étal du vendeur " + nomVendeur + ".");
 		question2.append("Bonjour " + nomAcheteur + "\n");
 		question2.append("Combien de " + produit + " voulez-vous acheter?\n");
 		quantiteProduit = Clavier.entrerEntier(question2.toString());
-		produitAchete= controlAcheterProduit.acheterProduit(etalVendeur, quantiteProduit);
-		System.out.println(nomAcheteur + " a acheté " + quantiteProduit + " de " + produit + " à " + nomVendeur + "." );
-		
+		produitAchete = controlAcheterProduit.acheterProduit(etalVendeur, quantiteProduit);
+		System.out.println(nomAcheteur + " a acheté " + quantiteProduit + " de " + produit + " à " + nomVendeur + ".");
 
 	}
 }
--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -12,19 +12,22 @@ public class BoundaryAfficherMarche {
 	public void afficherMarche(String nomAcheteur) {
 
 		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		String vendeur;
+		String quantite;
+		String produit;
 		if (infosMarche.length == 0) {
 			System.out.println("Le marché est vide, revenez plus tard.");
 		} else {
 			System.out.println(nomAcheteur + ", vous trouverez au marché :");
 			int j = 0;
-			while (j<infosMarche.length) {
-				String vendeur = infosMarche[j%3];
-				String quantite = infosMarche[(j%3) +1];
-				String produit = infosMarche[(j%3) + 2];
+			do {
+				vendeur = infosMarche[j % 3];
+				quantite = infosMarche[(j % 3) + 1];
+				produit = infosMarche[(j % 3) + 2];
 				System.out.println("-" + vendeur + " qui vend " + quantite + " " + produit);
-				j++;
-			}
-			
+				j += 3;
+			} while (j < infosMarche.length);
+
 		}
 	}
 }
