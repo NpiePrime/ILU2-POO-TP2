e1313525feacf8f4cc275392077919a5a589691c
Yanis Zerkani <zrn3963a@ens.fsi>
2023-04-11 15:26:43 +0200

    test Verifier identite

---
+++ test/controleur/ControlEmmenagerTest.java
@@ -0,0 +1,55 @@
+package controleur;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.*;
+import villagegaulois.*;
+
+class ControlEmmenagerTest {
+	private Village village;
+	private Chef abraracourcix;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irr�ductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+	}
+
+	@Test
+	void testControlEmmenager() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		assertNotNull(controlEmmenager, "Constructeur ne revoie pas null");
+	}
+
+	@Test
+	void testIsHabitant() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		assertTrue(controlEmmenager.isHabitant("Bonemine"));
+		assertFalse(controlEmmenager.isHabitant("Existe pas"));
+		controlEmmenager.ajouterDuide("Panoramix", 10, 1, 5);
+		assertTrue(controlEmmenager.isHabitant("Panoramix"));
+	}
+
+	@Test
+	void testAjouterDuide() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		controlEmmenager.ajouterDuide("Panoramix", 10, 1, 5);
+		assertTrue(controlEmmenager.isHabitant("Panoramix"));
+
+	}
+
+	@Test
+	void testAjouterGaulois() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		assertTrue(controlEmmenager.isHabitant("Bonemine"));
+	}
+
+}
---
+++ test/controleur/ControlPrendreEtalTest.java
@@ -0,0 +1,49 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlPrendreEtalTest {
+	private Village village;
+	private Chef abraracourcix;
+	private ControlVerifierIdentite controlVerifierIdentite;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irr�ductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+
+	}
+
+	@Test
+	void testControlPrendreEtal() {
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite, village);
+		assertNotNull(controlPrendreEtal, "Constructeur ne renvoie pas null");
+	}
+
+	@Test
+	void testResteEtals() {
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite, village);
+		assertTrue(controlPrendreEtal.resteEtals());
+	}
+
+	@Test
+	void testPrendreEtal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVerifierIdentite() {
+		ControlPrendreEtal controlPrendreEtal = new ControlPrendreEtal(controlVerifierIdentite, village);
+		assertTrue(controlPrendreEtal.verifierIdentite("Abraracourcix"));
+		assertFalse(controlPrendreEtal.verifierIdentite("N'existe pas"));
+	}
+
+}
